1d1277ec5ed41d1f952fce7c426603b03e1b80e5
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-20 19:28:14 +0100

    passport strategies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AdminsController } from './admins/admins.controller';
-import { AdminsService } from './admins/admins.service';
-import { AdminsModule } from './admins/admins.module'; 
-import config from './config/keys'; 
+import config from './config/keys';  
 
 @Module({
-  imports: [ItemsModule, MongooseModule.forRoot(config.mongoURI), AdminsModule],
-  controllers: [AdminsController],
-  providers: [AdminsService],
+  imports: [ItemsModule, MongooseModule.forRoot(config.mongoURI), ],  
+  controllers: [],  
+  providers: [],
 })
 export class AppModule {}
