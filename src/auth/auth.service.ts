ae73444d58ffd0d6848e2d824bf1e5252cbe5e17
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-21 09:40:47 +0100

    strategy file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { AdminsService } from 'src/admins/admins.service';
 @Injectable()
 export class AuthService {
-    constructor(private adminsService: AdminsService){}
+    constructor(private adminsService: AdminsService){} 
 
-    async validateAdmin(email: string, password: string): Promise<any>{
+    async validateAdmin(email: string, password: string): Promise<any>{ 
         const admin = await this.adminsService.findOne(email);
-        if(admin && admin.password === password){
+        if(admin && admin.password === password){ 
             const {password, email, ...rest} = admin;
             return rest;
         }
