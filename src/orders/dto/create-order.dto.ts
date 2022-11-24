a7d67d3aaba3c19d2ac5052175e2caa8d185cb4f
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-24 10:33:18 +0100

    dto order created

---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,7 @@
+export class CreateOrderDto{
+    readonly adresse: String;
+    readonly city: String;
+    readonly postal: String;
+    readonly id_client: String;
+    readonly id_car: String;
+}
