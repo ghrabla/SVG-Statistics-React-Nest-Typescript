6cc005c884adf3f2cb8761ac22bbb9578c0f4546
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-24 10:58:31 +0100

    test branches

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -2,6 +2,6 @@ export class CreateOrderDto{
     readonly adresse: String;
     readonly city: String;
     readonly postal: String;
-    readonly id_client: String;
+    readonly id_client: String; 
     readonly id_car: String;
 }
